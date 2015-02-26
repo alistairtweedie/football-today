56ad12203ede2c1fec68adbbf61525a066619fca
anthonyptetlow <anthonyptetlow@gmail.com>
2015-02-26 00:05:12 +0000

    Added Kick of date

--- client/src/js/module/controllers/FixtureController.js
+++ client/src/js/module/controllers/FixtureController.js
@@ -2,6 +2,11 @@ angular.module('football').controller('FixtureController', [
 	'todaysFixtures',
 	function (todaysFixtures) {
 		var fixtures = this;
+
+		todaysFixtures.fixtures.forEach(function(fixture){
+			fixture.date = new Date(fixture.date);
+		});
+
 		fixtures.todaysFixtures = todaysFixtures.fixtures;
 	}
 ]);
