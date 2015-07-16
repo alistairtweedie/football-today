04ab9718e49c77e3b7ceda804f14d19046bf4bf0
alistairtweedie <info@alistairtweedie.com>
2015-07-16 13:33:46 +0100

    Removed recess pipe
    Had problems with this so removed for now.

--- gulp/styles.js
+++ gulp/styles.js
@@ -5,11 +5,11 @@ var gulp = require('gulp'),
 
 gulp.task('styles', function() {
 	return gulp.src(__dirname + '/../client/src/less/**/*.less')
-		.pipe(recess())
+		/*.pipe(recess())
         .pipe(recess.reporter())
 		.on('error', function (error) {
 	        console.error(String(error));
-        })
+        })*/
 		.pipe(less())
 		.pipe(concat('core.css'))
 		.pipe(gulp.dest(__dirname + '/../public/styles/'));
