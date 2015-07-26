87d3d178c6c723d2f83f6824349508ed4505b718
anthonyptetlow <anthonyptetlow@gmail.com>
2015-07-26 19:13:37 +0100

    Moved from less to scss

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,17 +1,15 @@
 var gulp = require('gulp'),
-	less = require('gulp-less'),
-	concat = require('gulp-concat'),
-	recess = require('gulp-recess');
+    prefix = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename');
 
-gulp.task('styles', function() {
-	return gulp.src(__dirname + '/../client/src/less/**/*.less')
-		/*.pipe(recess())
-        .pipe(recess.reporter())
-		.on('error', function (error) {
-	        console.error(String(error));
-        })*/
-		.pipe(less())
-		.pipe(concat('core.css'))
-		.pipe(gulp.dest(__dirname + '/../public/styles/'));
-
+gulp.task('styles',  function() {
+  return gulp.src(__dirname + '/../client/src/scss/site.scss')
+        .pipe(sass())
+          .pipe(prefix())
+          .pipe(gulp.dest(__dirname + '/../public/styles'))
+          .pipe(minifyCSS())
+          .pipe(rename('site.min.css'))
+          .pipe(gulp.dest(__dirname + '/../public/styles/'));
 });
--- gulp/watch.js
+++ gulp/watch.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 gulp.task('watch', function () {
 	gulp.watch(__dirname + '/../client/src/js/**/*.js', ['script']);
 	gulp.watch(__dirname + '/../client/src/**/*.html', ['html']);
-	gulp.watch(__dirname + '/../client/src/less/**/*.less', ['styles']);
+	gulp.watch(__dirname + '/../client/src/less/**/*.less', ['stylesOld']);
+	gulp.watch(__dirname + '/../client/src/scss/**/*.scss', ['styles']);
 	gulp.watch(__dirname + '/../client/src/assets/*', ['assets']);
 });
